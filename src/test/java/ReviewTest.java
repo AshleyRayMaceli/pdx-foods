4fd58670eef6f60297963accf6ff3d55cbb92311
AdamCraig and ashleyraymaceli <ashleyraymaceli@>
2016-05-04 08:48:11 -0700

    add initial set up files and initialized database with tables

---
+++ src/test/java/ReviewTest.java
@@ -0,0 +1,22 @@
+
+import org.sql2o.*;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class ReviewTest {
+
+  @Before
+  public void setUp() {
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/pdx_foods_test", null, null);
+  }
+
+  @After
+  public void tearDown() {
+    try(Connection con = DB.sql2o.open()) {
+      String deleteReviewsQuery = "DELETE FROM reviews *;";
+      String deleteRestaurantsQuery = "DELETE FROM restaurants *;";
+      con.createQuery(deleteReviewsQuery).executeUpdate();
+      con.createQuery(deleteRestaurantsQuery).executeUpdate();
+    }
+  }
+}

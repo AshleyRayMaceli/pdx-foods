1ead7905cc2c81f53dee19082ee9c8ef72810ceb
AdamCraig and ashleyraymaceli <ashleyraymaceli@>
2016-05-04 11:41:50 -0700

    add starting and passing integration testing for rootTest (index.vtl) and began adding to restaurants.vtl

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -3,11 +3,29 @@ import org.junit.ClassRule;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.sql2o.*;
+import org.junit.*;
+import static org.fluentlenium.core.filter.FilterConstructor.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AppTest extends FluentTest {
   public WebDriver webDriver = new HtmlUnitDriver();
+
+  @Before
+  public void setUp() {
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/to_do_test", null, null);
+  }
+
+  @After
+  public void tearDown() {
+    try(Connection con = DB.sql2o.open()) {
+      String deleteTasksQuery = "DELETE FROM tasks *;";
+      String deleteCategoriesQuery = "DELETE FROM categories *;";
+      con.createQuery(deleteTasksQuery).executeUpdate();
+      con.createQuery(deleteCategoriesQuery).executeUpdate();
+    }
+  }
 
   @Override
   public WebDriver getDefaultDriver() {
@@ -19,7 +37,8 @@ public class AppTest extends FluentTest {
 
   @Test
   public void rootTest() {
-
+    goTo("http://localhost:4567/");
+    assertThat(pageSource()).contains("PDX Foods");
   }
 
 }
